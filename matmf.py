a12a1c17f017e2be9e049d608538b700b6469c1e
rdk31 <rdk31@protonmail.com>
2020-05-08 15:36:45 +0200

    check args

--- matmf.py
+++ matmf.py
@@ -5,4 +5,9 @@ import sys
 
 if __name__ == "__main__":
     c = compiler.Compiler()
+
+    if len(sys.argv) != 2:
+        print("python matmf.py <file.asm>")
+        sys.exit(1)
+
     c.compile(sys.argv[1])
